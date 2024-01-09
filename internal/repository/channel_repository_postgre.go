86c669d3a165f69a893890adb055ccf2480744c5
auful01 <distriq19@gmail.com>
2024-01-09 11:44:02 +0700

    Update

--- internal/repository/channel_repository_postgre.go
+++ internal/repository/channel_repository_postgre.go
@@ -342,7 +342,7 @@ func (c *ChannelRepositoryPostgre) Create(ctx context.Context, channel entity.Ch
 	}
 
 	// Check for duplicate channel_id
-	duplicateQuery := `SELECT id FROM channels WHERE channel_id = $1 LIMIT 1`
+	duplicateQuery := `SELECT id FROM channels WHERE channel_id = ? LIMIT 1`
 	fmt.Println("duplicateQuery", duplicateQuery)
 	var existingID int64
 	err := cmd.
