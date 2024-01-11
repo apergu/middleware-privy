e14b5ded165ced982a93b16f86a38463fccf361f
auful01 <distriq19@gmail.com>
2024-01-11 16:16:27 +0700

    Update

--- internal/usecase/merchant_usecase_command.go
+++ internal/usecase/merchant_usecase_command.go
@@ -84,7 +84,7 @@ func (r *MerchantCommandUsecaseGeneral) Create(ctx context.Context, merchant mod
 
 	privyParam := credential.MerchantParam{
 		RecordType:                  "customrecord_customer_hierarchy",
-		CustRecordCustomerName:      customer.CustomerID,
+		CustRecordCustomerName:      customer.CustomerName,
 		CustRecordMerchantID:        merchant.MerchantID,
 		CustRecordPrivyCodeMerchant: merchant.MerchantCode,
 		CustRecordMerchantName:      merchant.MerchantName,
