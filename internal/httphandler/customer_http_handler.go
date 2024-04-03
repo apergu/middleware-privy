16b2cf1d5fbedabec31065fc8149ae514fd9b400
Cecep Wandy <wandy.irawan17@gmail.com>
2024-04-03 17:17:36 +0700

    fix logic create lead net suit

--- internal/httphandler/customer_http_handler.go
+++ internal/httphandler/customer_http_handler.go
@@ -8,17 +8,17 @@ import (
 	"net/http"
 	"os"
 
-	"middleware/internal/constants"
-	"middleware/internal/model"
-	"middleware/internal/repository"
-	"middleware/internal/usecase"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
 	"gitlab.com/rteja-library3/rapperror"
 	"gitlab.com/rteja-library3/rdecoder"
 	"gitlab.com/rteja-library3/rhelper"
 	"gitlab.com/rteja-library3/rresponser"
+
+	"middleware/internal/constants"
+	"middleware/internal/model"
+	"middleware/internal/repository"
+	"middleware/internal/usecase"
 )
 
 type CustomerHttpHandler struct {
@@ -129,57 +129,98 @@ func (h CustomerHttpHandler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// if payload.CRMLeadID == "" {
-	url := os.Getenv("ACZD_BASE") + "api/v1/privy/zendesk/lead"
+	if payload.ValidateLogic() {
+		// if payload.CRMLeadID == "" {
+		url := os.Getenv("ACZD_BASE") + "api/v1/privy/zendesk/lead"
 
-	// Replace the following map with your actual data
-	data := map[string]interface{}{
-		"zd_lead_id":          payload.CRMLeadID,
-		"first_name":          payload.FirstName,
-		"last_name":           payload.LastName,
-		"enterprise_privy_id": payload.EnterprisePrivyID,
-		"enterprise_name":     payload.CustomerName,
-		"address":             payload.Address,
-		"email":               payload.Email,
-		"zip":                 payload.ZipCode,
-		"state":               payload.State,
-		"country":             "Indonesia",
-		"city":                payload.City,
-		"npwp":                payload.NPWP,
-	}
+		// Replace the following map with your actual data
+		data := map[string]interface{}{
+			"zd_lead_id":          payload.CRMLeadID,
+			"first_name":          payload.FirstName,
+			"last_name":           payload.LastName,
+			"enterprise_privy_id": payload.EnterprisePrivyID,
+			"enterprise_name":     payload.CustomerName,
+			"address":             payload.Address,
+			"email":               payload.Email,
+			"zip":                 payload.ZipCode,
+			"state":               payload.State,
+			"country":             "Indonesia",
+			"city":                payload.City,
+			"npwp":                payload.NPWP,
+		}
 
-	// Convert data to JSON
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		panic(err)
-	}
+		// Convert data to JSON
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
 
-	// Make the HTTP POST request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		response = rresponser.NewResponserError(err)
-		rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
+		// Make the HTTP POST request
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+		if err != nil {
+			response = rresponser.NewResponserError(err)
+			rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
+			return
+		}
+
+		req.Header.Add("Content-Type", "application/json")
+		req.SetBasicAuth(os.Getenv("BASIC_AUTH_USERNAME"), os.Getenv("BASIC_AUTH_PASSWORD"))
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			response = rresponser.NewResponserError(err)
+			rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		response = rresponser.NewResponserSuccessCreated("", "Customer successfully created", 2, map[string]interface{}{
+			"test": 200,
+		})
+	} else {
+		logrus.
+			WithFields(logrus.Fields{
+				"at":     "CustomerUsageHttpHandler.Create",
+				"src":    "payload.Validate",
+				"params": payload,
+			}).
+			Error(err)
+
+		errorResponse := map[string]interface{}{
+			"code":    422,
+			"success": false,
+			"message": "Validation logic failed",
+			"errors":  errors,
+		}
+
+		// Convert error response to JSON
+		responseJSON, marshalErr := json.Marshal(errorResponse)
+		if marshalErr != nil {
+			// Handle JSON marshaling error
+			fmt.Println("Error encoding JSON:", marshalErr)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// Set the response headers
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnprocessableEntity) // Set the appropriate HTTP status code
+
+		// Write the JSON response to the client
+		_, writeErr := w.Write(responseJSON)
+		if writeErr != nil {
+			// Handle write error
+			fmt.Println("Error writing response:", writeErr)
+		}
+
 		return
 	}
 
-	req.Header.Add("Content-Type", "application/json")
-	req.SetBasicAuth(os.Getenv("BASIC_AUTH_USERNAME"), os.Getenv("BASIC_AUTH_PASSWORD"))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		response = rresponser.NewResponserError(err)
-		rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
-		return
-	}
-	defer resp.Body.Close()
-
-	response = rresponser.NewResponserSuccessCreated("", "Customer successfully created", 2, map[string]interface{}{
-		"test": 200,
-	})
-	// } else {
-	if payload.CRMLeadID != "" {
-		if payload.EntityStatus == "13" {
+	//only status won and have crmLeadID send to NetSweet
+	if payload.CRMLeadID != nil && payload.EntityStatus != nil {
+		if *payload.EntityStatus == "13" {
 			log.Println("payload masuk 13", payload)
 			roleId, meta, err := h.Command.Create(ctx, payload)
 			if err != nil {
@@ -188,25 +229,7 @@ func (h CustomerHttpHandler) Create(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			response = rresponser.NewResponserSuccessCreated("", "Customer successfully created", roleId, meta)
-		} else {
-			log.Println("payload masuk 6", payload)
-			roleId, meta, err := h.Command.CreateLead2(ctx, payload)
-			if err != nil {
-				response = rresponser.NewResponserError(err)
-				rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
-				return
-			}
-			response = rresponser.NewResponserSuccessCreated("", "Customer successfully created", roleId, meta)
 		}
-	} else {
-		log.Println("CRM LEAD ID KOSONG", payload)
-		roleId, meta, err := h.Command.CreateLead2(ctx, payload)
-		if err != nil {
-			response = rresponser.NewResponserError(err)
-			rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
-			return
-		}
-		response = rresponser.NewResponserSuccessCreated("", "Customer successfully created", roleId, meta)
 	}
 
 	rdecoder.EncodeRestWithResponser(w, h.Decorder, response)
