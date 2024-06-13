2d7573b0b3a7028d0eea461c3eb619882345a525
auful01 <distriq19@gmail.com>
2024-06-13 13:46:58 +0700

    Update

--- internal/usecase/merchant_usecase_command.go
+++ internal/usecase/merchant_usecase_command.go
@@ -34,9 +34,39 @@ func (r *MerchantCommandUsecaseGeneral) Create(ctx context.Context, merchant mod
 		return 0, nil, err
 	}
 
+	defer func() {
+		if p := recover(); p != nil {
+			r.merchantRepo.RollbackTx(ctx, tx)
+			panic(p)
+		} else if err != nil {
+			log.Println("Rolling back transaction due to error:", err)
+			r.merchantRepo.RollbackTx(ctx, tx)
+		} else {
+			err = r.merchantRepo.CommitTx(ctx, tx)
+			if err != nil {
+				log.Println("Error committing transaction:", err)
+			}
+		}
+	}()
+
 	tmNow := time.Now().UnixNano() / 1000000
 
 	respCust, _ := r.merchantRepo.FindByName(ctx, merchant.MerchantName, tx)
+
+	defer func() {
+		if p := recover(); p != nil {
+			r.merchantRepo.RollbackTx(ctx, tx)
+			panic(p)
+		} else if err != nil {
+			log.Println("Rolling back transaction due to error:", err)
+			r.merchantRepo.RollbackTx(ctx, tx)
+		} else {
+			err = r.merchantRepo.CommitTx(ctx, tx)
+			if err != nil {
+				log.Println("Error committing transaction:", err)
+			}
+		}
+	}()
 
 	if respCust.MerchantName != "" {
 		return 0, nil, rapperror.ErrConflict(
