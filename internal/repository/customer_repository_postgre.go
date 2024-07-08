b5adf02c33d367e838429ade62a5abc4f3696ad9
auful01 <distriq19@gmail.com>
2024-07-09 06:50:54 +0700

    Update

--- internal/repository/customer_repository_postgre.go
+++ internal/repository/customer_repository_postgre.go
@@ -541,7 +541,7 @@ func (c *CustomerRepositoryPostgre) Create(ctx context.Context, cust entity.Cust
 		created_by, created_at, updated_by, updated_at, entitystatus
 	) values (
 		$1, $2, $3, $4, $5, $6, $7, $8, $9, $10
-		,$11, $12 ,$13, $14, $15, $16, $17, $18, $19, $20, $21, $22
+		,$11, $12 ,$13, $14, $15, $16, $17, $18, $19, $20, $21
 	) RETURNING id`
 
 	fmt.Println("============== ID ==============", cust)
